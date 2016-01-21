c803c80a975892d437882de64c282599ccd854fe
Stanley Quon <stanleyq@yahoo.com>
2016-01-21 23:37:53 +0000

    More details in index.html

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,6 +1,10 @@
 package 'httpd'
 file '/var/www/html/index.html' do
-   content "<h1>Hello, world!</h1>\n"
+   content "<h1>Hello, world!
+   IPADDRESS:  #{node['ipaddress']}
+   HOSTNAME:   #{node['hostname']}
+   MEMORY:     #{node['memory']['total']}
+   CPU:        #{node['cpu']['0']['mhz']}</h1>\n"
 end
 service "httpd" do
    action [ :enable, :start ]
