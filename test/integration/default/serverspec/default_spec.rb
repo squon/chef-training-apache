3b7370f27021134b97857c71f4671bbe4cb5a207
Stanley Quon <stanleyq@yahoo.com>
2016-01-21 22:50:07 +0000

    Syntax boo-boo

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe 'apache::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  # it 'does something' do
+    # skip 'Replace this with meaningful tests'
+  # end
+  describe package ("httpd") do
+    it {should be_installed}
+  end
+  describe command ("ls -o /tmp") do
+     its (:exit_status) { should eq 0 }
   end
 end
